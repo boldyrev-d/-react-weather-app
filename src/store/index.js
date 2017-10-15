8ecfea23f00c9e7b5bdedbe98feea7ea427e0b4d
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-10-15 21:55:37 +0300

    chore: add redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import reducer from '../reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = [thunk];
 
@@ -9,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
   enhancer.push(logger);
 }
 
-const store = createStore(reducer, applyMiddleware(...enhancer));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(...enhancer)));
 
 if (process.env.NODE_ENV === 'development') {
   window.store = store;
